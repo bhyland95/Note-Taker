646adb85e99c641e7e6d473ca1496100de354f4f
Bhyland <blakehyland95@gmail.com>
2021-10-08 13:22:34 -0600

    added POST route

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,10 +1,37 @@
 const router = require("express").Router();
-const notesData  = require("../../db/db.json");
+const fs = require("fs");
+const notesData   = require("../../db/db.json");
 
-
+//returns all notes
 router.get("/notes", (req, res) => {
     res.json(notesData);
 });
 
+function createNewNote(notes){
+    // Converts new JSON Array back to string
+    notes = JSON.stringify(notes);
+    // Writes String back to db.json
+    fs.writeFileSync("./db/db.json", notes, function(err){
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
+
+//adds note to data
+router.post("/notes", (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notesData.length.toString();
+
+    
+    notesData.push(req.body)
+    
+    // Write notes data to database
+    createNewNote(notesData);
+    console.log(notesData);
+
+    // returns new note in JSON format.
+    res.json(req.body);
+});
 
 module.exports = router;
